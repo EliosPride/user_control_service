32f4aa4c695c5395059118f67822e5f5bc036084
Illia Sydorchuk <batonvagon@gmail.com>
2023-09-27 18:36:17 +0300

    Initial commit

---
+++ src/main/java/com/elios/user_control_service/UserControlServiceApplication.java
@@ -0,0 +1,15 @@
+package com.elios.user_control_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserControlServiceApplication
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(UserControlServiceApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/elios/user_control_service/UserControlServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.elios.user_control_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserControlServiceApplicationTests
+{
+    
+    @Test
+    void contextLoads()
+    {
+    }
+    
+}
